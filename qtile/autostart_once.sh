79e8d584831b380fc02780ff3478d99a924b6a56
Srinivasan T M <tmsrinivasantm@gmail.com>
2024-09-03 22:30:30 +0530

    Added nm-applet in startup

--- qtile/autostart_once.sh
+++ qtile/autostart_once.sh
@@ -4,4 +4,5 @@ var=$(xinput --list | grep 'MSFT0001:01 04F3:309E Touchpad' | awk '{print $6}' |
 xinput --set-prop "$var" 'libinput Natural Scrolling Enabled' 1
 xinput --set-propo "$var" 'libinput Tapping Enabled' 1
 picom -b --config ~/.config/picom/picom.conf &
+nm-applet &
 dunst &
