4f9459222d1059c5f5b546f5061a9b6425db1b78
Srinivasan T M <tmsrinivasantm@gmail.com>
2024-09-29 15:17:36 +0530

    Added bar similar to gnome bar

--- qtile/config.py
+++ qtile/config.py
@@ -156,9 +156,9 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="sans",
-    fontsize=9.5,
-    padding=3,
+    font="Noto Sans Black",
+    fontsize=12.5,
+    # padding=3,
 )
 extension_defaults = widget_defaults.copy()
 
@@ -166,30 +166,35 @@ screens = [
     Screen(
         wallpaper='~/Pictures/wallpapers/Decay-Green/moments_before_desk.png',
         wallpaper_mode='stretch',
-        # bottom=bar.Bar(
-        #     [
-        #         widget.CurrentLayout(),
-        #         widget.GroupBox(),
-        #         widget.Prompt(),
-        #         widget.WindowName(),
-        #         widget.Chord(
-        #             chords_colors={
-        #                 "launch": ("#ff0000", "#ffffff"),
-        #             },
-        #             name_transform=lambda name: name.upper(),
-        #         ),
-        #         widget.TextBox("default config", name="default"),
-        #         widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-        #         # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-        #         # widget.StatusNotifier(),
-        #         widget.Systray(),
-        #         widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-        #         widget.QuickExit(),
-        #     ],
-        #     24,
-        #     # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-        #     # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-        # ),
+        top=bar.Bar(
+            [
+                # widget.CurrentLayout(),
+                # widget.GroupBox(),
+                # widget.Prompt(),
+                # widget.WindowName(),
+                # widget.Chord(
+                #     chords_colors={
+                #         "launch": ("#ff0000", "#ffffff"),
+                #     },
+                #     name_transform=lambda name: name.upper(),
+                # ),
+                # widget.TextBox("default config", name="default"),
+                # widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+                # widget.StatusNotifier(),
+                widget.TextBox("", name="default", ),
+                widget.Spacer(),
+                widget.Clock(format="%a %I:%M %p"),
+                widget.Spacer(),
+                widget.Systray(),
+                widget.Spacer(length=10),
+                widget.Clock(format="%Y-%m-%d"),
+                # widget.QuickExit(),
+            ],
+            20,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
         # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
         # By default we handle these events delayed to already improve performance, however your system might still be struggling
         # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
