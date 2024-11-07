0c8489ab039ba3c6d48b528371458cc7943f1fe2
Srinivasan T M <tmsrinivasantm@gmail.com>
2024-11-07 21:55:22 +0530

    Using monad tall layout (master layout)

--- qtile/config.py
+++ qtile/config.py
@@ -143,9 +143,9 @@ layouts = [
     # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
-     layout.Bsp(border_focus="#4e9170", margin=10, border_width=1, border_on_single=True),
+     # layout.Bsp(border_focus="#4e9170", margin=10, border_width=1, border_on_single=True),
     # layout.Matrix(),
-    # layout.MonadTall(border_focus="#4e9170", margin=10, border_width=1, border_width_single=1),
+    layout.MonadTall(border_focus="#4e9170", margin=10, border_width=1, border_width_single=1),
     # layout.MonadWide(),
     # layout.RatioTile(),
     # layout.Tile(),
