1b6e80db7caf10e6d7323e0ce82901efcee20e7d
Srinivasan T M <tmsrinivasantm@gmail.com>
2025-03-04 12:24:16 +0530

    using catpuccin machiatto theme

--- lvim/config.lua
+++ lvim/config.lua
@@ -4,104 +4,109 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
 lvim.plugins = { {
-        'tpope/vim-fugitive' },
+    'tpope/vim-fugitive' },
     {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        config = function()
-            require("rose-pine").setup({
-                variant = "auto", -- auto, main, moon, or dawn
-                dark_variant = "main", -- main, moon, or dawn
-                dim_inactive_windows = false,
-                extend_background_behind_borders = true,
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
+    -- {
+    --     'rose-pine/neovim',
+    --     name = 'rose-pine',
+    --     config = function()
+    --         require("rose-pine").setup({
+    --             variant = "auto",      -- auto, main, moon, or dawn
+    --             dark_variant = "main", -- main, moon, or dawn
+    --             dim_inactive_windows = false,
+    --             extend_background_behind_borders = true,
 
-                enable = {
-                    terminal = true,
-                    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-                    migrations = true, -- Handle deprecated options automatically
-                },
-                styles = {
-                    bold = true,
-                    italic = true,
-                    transparency = true,
-                },
+    --             enable = {
+    --                 terminal = true,
+    --                 legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+    --                 migrations = true,        -- Handle deprecated options automatically
+    --             },
+    --             styles = {
+    --                 bold = true,
+    --                 italic = true,
+    --                 transparency = true,
+    --             },
 
-                groups = {
-                    border = "muted",
-                    link = "iris",
-                    panel = "surface",
+    --             groups = {
+    --                 border = "muted",
+    --                 link = "iris",
+    --                 panel = "surface",
 
-                    error = "love",
-                    hint = "iris",
-                    info = "foam",
-                    note = "pine",
-                    todo = "rose",
-                    warn = "gold",
+    --                 error = "love",
+    --                 hint = "iris",
+    --                 info = "foam",
+    --                 note = "pine",
+    --                 todo = "rose",
+    --                 warn = "gold",
 
-                    git_add = "foam",
-                    git_change = "rose",
-                    git_delete = "love",
-                    git_dirty = "rose",
-                    git_ignore = "muted",
-                    git_merge = "iris",
-                    git_rename = "pine",
-                    git_stage = "iris",
-                    git_text = "rose",
-                    git_untracked = "subtle",
+    --                 git_add = "foam",
+    --                 git_change = "rose",
+    --                 git_delete = "love",
+    --                 git_dirty = "rose",
+    --                 git_ignore = "muted",
+    --                 git_merge = "iris",
+    --                 git_rename = "pine",
+    --                 git_stage = "iris",
+    --                 git_text = "rose",
+    --                 git_untracked = "subtle",
 
-                    h1 = "iris",
-                    h2 = "foam",
-                    h3 = "rose",
-                    h4 = "gold",
-                    h5 = "pine",
-                    h6 = "foam",
-                },
+    --                 h1 = "iris",
+    --                 h2 = "foam",
+    --                 h3 = "rose",
+    --                 h4 = "gold",
+    --                 h5 = "pine",
+    --                 h6 = "foam",
+    --             },
 
-                pallete = {
-                    -- Override the builtin palette per variant
-                    -- moon = {
-                    --     base = '#18191a',
-                    --     overlay = '#363738',
-                    -- },
-                },
+    --             pallete = {
+    --                 -- Override the builtin palette per variant
+    --                 -- moon = {
+    --                 --     base = '#18191a',
+    --                 --     overlay = '#363738',
+    --                 -- },
+    --             },
 
-                highlight_groups = {
-                    Comment = { fg = "#575757" },
-                    -- VertSplit = { fg = "muted", bg = "muted" },
-                },
+    --             highlight_groups = {
+    --                 Comment = { fg = "#575757" },
+    --                 -- VertSplit = { fg = "muted", bg = "muted" },
+    --             },
 
-                before_highlight = function(group, highlight, palette)
-                    -- Disable all undercurls
-                    -- if highlight.undercurl then
-                    --     highlight.undercurl = false
-                    -- end
-                    --
-                    -- Change palette colour
-                    -- if highlight.fg == palette.pine then
-                    --     highlight.fg = palette.foam
-                    -- end
-                end,
-            })
+    --             before_highlight = function(group, highlight, palette)
+    --                 -- Disable all undercurls
+    --                 -- if highlight.undercurl then
+    --                 --     highlight.undercurl = false
+    --                 -- end
+    --                 --
+    --                 -- Change palette colour
+    --                 -- if highlight.fg == palette.pine then
+    --                 --     highlight.fg = palette.foam
+    --                 -- end
+    --             end,
+    --         })
 
-            vim.cmd("colorscheme rose-pine")
-            -- vim.cmd("colorscheme rose-pine-main")
-            -- vim.cmd("colorscheme rose-pine-moon")
-            -- vim.cmd("colorscheme rose-pine-dawn")
-        end,
-    }
+    --         vim.cmd("colorscheme rose-pine")
+    --         -- vim.cmd("colorscheme rose-pine-main")
+    --         -- vim.cmd("colorscheme rose-pine-moon")
+    --         -- vim.cmd("colorscheme rose-pine-dawn")
+    --     end,
+    -- }
 }
 
 ------------------------------------------------------------ AutoCommands ---------------------------------------------------------------
 
-vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = "*",
-	callback = function()
-        vim.api.nvim_set_hl(0, "Visual", { bg = "#56526e" })
-        vim.api.nvim_set_hl(0, "LspReferenceText", { bg = "#2E3440" })
-        vim.api.nvim_set_hl(0, "LspReferenceRead", { bg = "#2E3440" })
-        vim.api.nvim_set_hl(0, "LspReferenceWrite", { bg = "#2E3440" })
-	end,
-})
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+--     pattern = "*",
+--     callback = function()
+--         vim.api.nvim_set_hl(0, "Visual", { bg = "#56526e" })
+--         vim.api.nvim_set_hl(0, "LspReferenceText", { bg = "#2E3440" })
+--         vim.api.nvim_set_hl(0, "LspReferenceRead", { bg = "#2E3440" })
+--         vim.api.nvim_set_hl(0, "LspReferenceWrite", { bg = "#2E3440" })
+--     end,
+-- })
 
 ------------------------------------------------------------ Keybinds -------------------------------------------------------------------
 
@@ -115,7 +120,7 @@ lvim.builtin.telescope.defaults = {
 lvim.builtin.indentlines.options.show_current_context = false;
 lvim.builtin.which_key.mappings['w'] = {}
 lvim.transparent_window = true;
-lvim.colorscheme = 'rose-pine-main'
+lvim.colorscheme = 'catppuccin'
 
 -- c to blackhole
 lvim.keys.normal_mode['c'] = '"_c'
@@ -158,7 +163,7 @@ lvim.builtin.which_key.mappings['g'] = {
     g = { "<cmd>vertical G<CR>", "Git status" },
     B = { "<cmd>G blame<cr>", "git blame" },
     r = { "<cmd>Gitsigns reset_hunk<cr>", "revert hunk" },
-    s = { "<cmd>Gitsigns stage_hunk<cr>", "stage hunk"}
+    s = { "<cmd>Gitsigns stage_hunk<cr>", "stage hunk" }
 }
 
 vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
