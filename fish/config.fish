d663c2246203f474e1959e91b4a45f70c2c2b867
tmsrinivasantm <tmsrinivasantm@gmail.com>
2025-12-16 21:13:01 +0530

    logout condition -> sway:wlroots

--- fish/config.fish
+++ fish/config.fish
@@ -17,7 +17,7 @@ if status is-interactive # Commands to run in interactive sessions can go here
     	alias reboot "systemctl reboot"
     else if [ $XDG_CURRENT_DESKTOP = "QTILE" ]
     	alias logout "qtile cmd-obj -o cmd -f shutdown"
-    else if [ $XDG_CURRENT_DESKTOP = "sway" ]
+    else if [ $XDG_CURRENT_DESKTOP = "sway:wlroots" ]
     	alias logout "sway exit"
     else if [ $XDG_CURRENT_DESKTOP = "GNOME" ]
     	alias logout "gnome-session-quit --no-prompt --logout"
